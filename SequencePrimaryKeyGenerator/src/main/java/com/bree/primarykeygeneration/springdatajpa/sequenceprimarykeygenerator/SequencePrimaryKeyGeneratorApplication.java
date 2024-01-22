0d3e2fbc48d55565424aa9205ed0711f78ca1f27
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-01-22 14:12:57 +0300

    GenerationType.SEQUENCE strategy for generating the primary key

---
+++ SequencePrimaryKeyGenerator/src/main/java/com/bree/primarykeygeneration/springdatajpa/sequenceprimarykeygenerator/SequencePrimaryKeyGeneratorApplication.java
@@ -0,0 +1,14 @@
+package com.bree.primarykeygeneration.springdatajpa.sequenceprimarykeygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SequencePrimaryKeyGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                SequencePrimaryKeyGeneratorApplication.class, args);
+    }
+
+}
