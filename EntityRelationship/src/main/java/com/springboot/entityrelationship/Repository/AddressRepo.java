40186943f00f9e04f903b7a6a8d3c8e0b6dbd44e
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-04-04 11:33:01 +0300

    working on Jpa relationships

--- EntityRelationship/src/main/java/com/springboot/entityrelationship/Repository/AddressRepo.java
+++ EntityRelationship/src/main/java/com/springboot/entityrelationship/Repository/AddressRepo.java
@@ -1,10 +1,11 @@
 package com.springboot.entityrelationship.Repository;
 
-import com.springboot.entityrelationship.Entity.Address;
+//import com.springboot.entityrelationship.Entity.Address;
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AddressRepo extends JpaRepository<Address, Long> {
+public interface AddressRepo extends JpaRepository<RabbitConnectionDetails.Address,Long>{
 
 }
--- EntityRelationship/src/main/java/com/springboot/entityrelationship/Repository/EmployeeRepo.java
+++ EntityRelationship/src/main/java/com/springboot/entityrelationship/Repository/EmployeeRepo.java
@@ -1,10 +1,10 @@
 package com.springboot.entityrelationship.Repository;
 
-import com.springboot.entityrelationship.Entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepo extends JpaRepository<Employee,Long> {
+
 
 }
