4ce7198a74ce2735584e2849a44c8e72cd31af33
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-04-04 10:58:32 +0300

    working on DataJpa relationships

---
+++ EntityRelationship/src/main/java/com/springboot/entityrelationship/EntityRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.entityrelationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityRelationshipApplication.class, args);
+	}
+
+}
