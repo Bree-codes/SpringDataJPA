e501c83fde2f5d8d73f8da2bab36876045f37282
Bree-codes <mwongera.mukami22@students.dkut.ac.ke>
2024-04-04 11:10:41 +0300

    working on DataJpa relationships

--- EntityRelationship/src/main/java/com/springboot/entityrelationship/Entity/Address.java
+++ EntityRelationship/src/main/java/com/springboot/entityrelationship/Entity/Address.java
@@ -1,4 +1,23 @@
 package com.springboot.entityrelationship.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "Address_table")
 public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Long id;
+
+    private String city;
+    private String addressType;
+
 }
--- EntityRelationship/src/main/java/com/springboot/entityrelationship/Entity/Employee.java
+++ EntityRelationship/src/main/java/com/springboot/entityrelationship/Entity/Employee.java
@@ -1,4 +1,28 @@
 package com.springboot.entityrelationship.Entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "employee_table")
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "employee_id")
+    private Long id;
+
+    private String name;
+    private int age;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_address_id")
+    private Address address;
+
 }
+
